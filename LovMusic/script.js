13dcac845052254cc03b6ed7bbfff6a02408a84f
Alex Machado <136132781+Alexaraujo10@users.noreply.github.com>
2024-05-10 04:12:28 -0300

    att

---
+++ LovMusic/script.js
@@ -0,0 +1,11 @@
+
+function carregar() {
+    var data = new Date();
+    var hora = data.getHours();
+
+    if (hora >= 18 || hora < 6) {
+        
+        document.body.style.background = '#45137eb9'
+
+    } 
+}
